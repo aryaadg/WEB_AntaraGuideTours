0f6e7758c8ebe0afdd8ecc4cc53da7913563ab4b
arya <aryaadiguna12@gmail.com>
2025-05-30 16:33:51 +0800

    tes
    rau

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Alex Travels",
+  title: "Antara Guide Tours",
   description: "Discover amazing travel destinations",
 }
 
